f19f73adb49b5ed543ab55cac4e91b887c48d2d0
Monida <monica.daniela.gomez@gmail.com>
2019-07-24 18:12:02 -0400

    Added the function return_query()

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return render_template('enterquery.html')
+
+@app.route('/show_analysis')
+def return_query():
+	return "Hello again!"
  
 if __name__ == '__main__':
 	app.run(debug=True)
