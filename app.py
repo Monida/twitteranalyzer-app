9a9f32f247bd18584cfbade2ac89a197f90d0e9e
Monida <monica.daniela.gomez@gmail.com>
2019-07-24 17:20:14 -0400

    Changed file name from enterquery back to index_hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-	return render_template('enterquery.html')
+	return render_template('index_hello.html')
  
 if __name__ == '__main__':
 	app.run()
