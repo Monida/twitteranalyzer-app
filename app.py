a3a9052b59ee348ac25bc07325e012fff0bb3ef9
Monida <monica.daniela.gomez@gmail.com>
2019-07-24 20:54:42 -0400

    Now it renders the returnquery.html

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
 
 @app.route('/show_analysis',methods=["POST"])
 def return_query():
-	return "Sorry this app doesn't analyze tweets yet. But wouldn't it be cool if it did?"
+	return render_template('returnquery.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
