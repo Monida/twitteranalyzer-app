b094f897c404736a4eda4e5b2a8b333d9fedc7de
Monida <monica.daniela.gomez@gmail.com>
2019-07-25 12:42:13 -0400

    Added code to plot polarity

--- app.py
+++ app.py
@@ -23,6 +23,11 @@ def return_query():
 
 		topics=find_topics(tweets)
 
+		# Plot polarity
+		fig=twitter_analyzer.create_figure(tweets)
+		fig.savefig('static/polarity_distribution.png')
+
+
 		return render_template('returnquery.html', query=app.vars['query'], 
 			num_of_tweets=app.vars['num_of_tweets'],table=topics.to_html())
 
