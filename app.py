829806225da47a1f0020d3d413739a1afc78a3dc
Monida <monica.daniela.gomez@gmail.com>
2019-07-24 21:01:55 -0400

    It takes the query word from enterqueryform and renders it to returnquery.html

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
- 
+app.vars={}
+
 @app.route('/')
 def hello_world():
 	return render_template('enterquery.html')
 
 @app.route('/show_analysis',methods=["POST"])
 def return_query():
-	return render_template('returnquery.html')
+	app.vars['query']=request.form['query']
+	return render_template('returnquery.html',query='query')
 
 if __name__ == '__main__':
 	app.run(debug=True)
