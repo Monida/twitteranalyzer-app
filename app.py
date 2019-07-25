c0a986c5ca1e943b002adf6fefcfb544a18448e8
Monida <monica.daniela.gomez@gmail.com>
2019-07-25 10:48:13 -0400

    Added num_of_tweets rendered to returnquery.html

--- app.py
+++ app.py
@@ -16,17 +16,20 @@ def return_query():
 		app.vars['query']=request.form['query']
 
 		# Send query to analyze
+		app.vars['num_of_tweets']=analyze_tweets(app.vars['query'])
 
-
-		return render_template('returnquery.html', query=app.vars['query'])
+		return render_template('returnquery.html', query=app.vars['query'], 
+			num_of_tweets=app.vars['num_of_tweets'])
 
 def clean_search():
 	app.vars['query']=''
 	return None
 
-def analyze_tweets():
-	pass
-	return None
+def analyze_tweets(query):
+	import twitter_analyzer
+	tweets=twitter_analyzer.get_tweets()
+	num_of_tweets=len(tweets)
+	return num_of_tweets
 
 
 if __name__ == '__main__':
