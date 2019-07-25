a09edef0785313055c098a02fe465b9badd56904
Monida <monica.daniela.gomez@gmail.com>
2019-07-25 11:41:50 -0400

    Added functions to find top_topics and polarity

--- app.py
+++ app.py
@@ -17,7 +17,11 @@ def return_query():
 		app.vars['query']=request.form['query']
 
 		# Send query to analyze
-		app.vars['num_of_tweets']=analyze_tweets(app.vars['query'])
+		tweets=analyze_tweets(app.vars['query'])
+
+		app.vars['num_of_tweets']=len(tweets)
+
+		topics=find_topics(tweets)
 
 		return render_template('returnquery.html', query=app.vars['query'], num_of_tweets=app.vars['num_of_tweets'])
 
@@ -28,9 +32,38 @@ def clean_search():
 def analyze_tweets(query):
 	import twitter_analyzer
 	tweets=twitter_analyzer.get_tweets(query)
-	num_of_tweets=len(tweets)
-	return num_of_tweets
+	tweets=twitter_analyzer.clean_and_tokenize(tweets)
+	tweets=twitter_analyzer.manualModelling(tweets)
+	return tweets
 
+def find_topics(tweets):
+	#Identify top topics
+	top_topics=twitter_analyzer.top_topics(tweets)
+
+	positives=tweet_polarity(tweets,top_topics,'positive')
+	neutrals=tweet_polarity(tweets,top_topics,'neutral')
+	negatives=tweet_polarity(tweets,top_topics,'negative')
+
+	topics=pd.DataFrame({'Topics':top_topics,
+		'Positive%':positives,
+		'Neutral%':neutrals,
+		'Negative%':negatives})
+	return topics
+
+def tweet_polarity(tweets,topics_list,polarity):
+	polarity_percent=[]
+
+	if len(topics_list)==0:
+		polarity_percent =[0]*len(topics_list)
+	else:
+		for topic in topics_list:
+			tweets_by_topic=tweets[tweets['topic_1']==topic]
+			tweets_by_polar=tweets_by_topic[tweets_by_topic['polarity_label']==polarity]
+			if len(tweets_by_polar)!=0:
+				polarity_percent.append(round(len(tweets_by_polar)/len(tweets_by_topic),2))
+			else:
+				polarity_percent.append(0)
+	return polarity_percent
 
 if __name__ == '__main__':
 	app.run(debug=True) 
