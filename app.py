d55b654321d4b377ec18d9ca5496407b1d861c90
Monida <monica.daniela.gomez@gmail.com>
2019-07-24 21:06:26 -0400

    Removed the asignation of the query as an app.vars

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def hello_world():
 
 @app.route('/show_analysis',methods=["POST"])
 def return_query():
-	app.vars['query']=request.form['query']
 	return render_template('returnquery.html')
 
 if __name__ == '__main__':
