ffec1d34b1fa28f848896c94a23c308ead7ad388
Monida <monica.daniela.gomez@gmail.com>
2019-07-25 09:58:21 -0400

    Change the function name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app.vars={}
 
 @app.route('/')
-def hello_world():
+def enter_query():
 	return render_template('enterquery.html')
 
 @app.route('/show_analysis',methods=["GET","POST"])
@@ -14,6 +14,7 @@ def return_query():
 		return redirect('/')
 	if request.method=="POST":
 		app.vars['query']=request.form['query']
+
 		return render_template('returnquery.html', query=app.vars['query'])
 
 def clean_search():
