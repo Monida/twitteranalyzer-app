f687ba5757d3d7d1d51cff898f741785ab52eebc
Monida <monica.daniela.gomez@gmail.com>
2019-07-24 21:57:27 -0400

    Implemented the clean_search function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template, request
 
 app = Flask(__name__)
 app.vars={}
+app.vars['query']=''
 
 @app.route('/',methods=["GET","POST"])
 def hello_world():
@@ -9,7 +10,7 @@ def hello_world():
 		return render_template('enterquery.html')
 	if request.method=="POST":
 		if app.vars['query']!='':
-			app.vars['query']==''
+			clean_search()
 		return render_template('enterquery.html')
 
 @app.route('/show_analysis',methods=["POST"])
