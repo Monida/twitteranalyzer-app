f4397f5a63526138e6195979f62311791acc4b28
Monica <monica.daniela.gomez@gmail.com>
2019-09-10 15:54:35 -0500

    Debugging request timeout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 import pandas as pd
 from twitter_analyzer.Twitter import Twitter
 import os
@@ -8,41 +8,43 @@ from worker import conn
 from redis import Redis
 import time
 
-q= Queue('low',connection = conn)
-
+q = Queue('low', connection=conn)
 
 app = Flask(__name__)
-app.vars={}
-twitter=Twitter('')
+app.vars = {}
+twitter = Twitter('')
 
-@app.route('/',methods=["GET","POST"])
+
+@app.route('/', methods=["GET", "POST"])
 def enter_query():
-	if request.method=="GET":
-		return render_template('enterquery.html')
-	else:
-		clean_search()
-		return render_template('enterquery.html')
+    if request.method == "GET":
+        return render_template('enterquery.html')
+    else:
+        clean_search()
+        return render_template('enterquery.html')
 
-@app.route('/show_analysis',methods=["GET","POST"])
+
+@app.route('/show_analysis', methods=["GET", "POST"])
 def return_query():
-	if request.method=="GET":
-		# Coming from moreinsights.html
-		return render_template('returnquery.html', query=app.vars['query'], num_of_tweets=app.vars['num_of_tweets'],
-				table=twitter.top_words['words'].to_html(index=False,header=False))
-	if request.method=="POST":
-		# Coming from enterquery.html
-    		if request.form['query']=='':
-    			return redirect('/')
-    		else:
-	        	app.vars['query']=request.form['query']
-	        
-	        	# Get tweets
-	        	twitter.query=app.vars['query']
-	        
-		        # Send get_tweets task to background worker
-		        #job=q.enqueue('twitter.get_tweets()')
-		        background_worker()
-		        return render_template('analyzing.html')
+    if request.method == "GET":
+        # Coming from moreinsights.html
+        return render_template('returnquery.html', query=app.vars['query'], num_of_tweets=app.vars['num_of_tweets'],
+                               table=twitter.top_words['words'].to_html(index=False, header=False))
+    if request.method == "POST":
+        # Coming from enterquery.html
+        if request.form['query'] == '':
+            return redirect('/')
+        else:
+            app.vars['query'] = request.form['query']
+
+            # Get tweets
+            twitter.query = app.vars['query']
+
+            # Send get_tweets task to background worker
+            # job=q.enqueue('twitter.get_tweets()')
+            background_worker()
+            return render_template('analyzing.html')
+
 
 '''
 Sep 9 2019
@@ -87,95 +89,98 @@ Sep 9 2019
 					clustergram=twitter.clustergram)
 '''
 
-def background_worker():
-	job=q.enqueue('twitter.get_tweets()')
-	analyze_tweets(job)
 
+def background_worker():
+	job = q.enqueue('twitter.get_tweets()')
+	analyze_tweets(job)
 	return None
 
 @app.route('/analyzing')
 def analyze_tweets(job_results):
-	#Reformat tweets
-	tweets=twitter.reformat_tweets(job_results)
+	# Reformat tweets
+	tweets = twitter.reformat_tweets(job_results)
 
-	# Check for empty data frame
-	if tweets.empty == True:
+	if tweets.empty:
 		return redirect('/error')
 
-	# Clean tweets
-	tweets=twitter.clean_and_tokenize()
 
-	app.vars['num_of_tweets']=len(tweets)
+	# Clean tweets
+	tweets = twitter.clean_and_tokenize()
+
+	app.vars['num_of_tweets'] = len(tweets)
 
 	# Perform topic modelling
-	tweets=twitter.manualModelling()
+	tweets = twitter.manualModelling()
 
-	vectorized_tweets=twitter.vectorize_tweets()
+	vectorized_tweets = twitter.vectorize_tweets()
 
-	matrix=vectorized_tweets['words_matrix']
+	matrix = vectorized_tweets['words_matrix']
 
-	feature_names=vectorized_tweets['feature_names']
+	feature_names = vectorized_tweets['feature_names']
 
-	LDA_model=twitter.fit_LDA(matrix)
+	lda_model = twitter.fit_LDA(matrix)
 
-	twitter.LDA_top_words(LDA_model,feature_names)
+	twitter.LDA_top_words(lda_model, feature_names)
 
 	# Plot clustergram
 	twitter.top_labeled_topics()
-	clusters_data=twitter.cluster_text(
+	clusters_data = twitter.cluster_text()
 	twitter.create_clustergram(twitter.topics)
+	topic_table = twitter.top_words['words']
 
-	return render_template('returnquery.html',query=app.vars['query'],num_of_tweets=app.vars['num_of_tweets'],table=twitter.top_words['words'].to_html(index=False, header=False),clustergram=twitter.clustergram)
-
-
+	return render_template('returnquery.html', query=app.vars['query'],
+						   num_of_tweets=app.vars['num_of_tweets'],
+						   table=topic_table.to_html(index=False, header=False),
+						   clustergram=twitter.clustergram)
 
 
 @app.route('/moreinsights', methods=["POST"])
 def more_insights():
+    # Check what button was clicked on returnquery.html
+    if request.form['insights'] == 'topic1':
+        tweets_per_topic = twitter.topics['Count'][0]
+        selected_topic = twitter.topics.index[0]
+    elif request.form['insights'] == 'topic2':
+        tweets_per_topic = twitter.topics['Count'][1]
+        selected_topic = twitter.topics.index[1]
+    elif request.form['insights'] == 'topic3':
+        tweets_per_topic = twitter.topics['Count'][2]
+        selected_topic = twitter.topics.index[2]
+    elif request.form['insights'] == 'topic4':
+        tweets_per_topic = twitter.topics['Count'][3]
+        selected_topic = twitter.topics.index[3]
+    else:
+        tweets_per_topic = twitter.topics['Count'][4]
+        selected_topic = twitter.topics.index[4]
 
-	# Check what button was clicked on returnquery.html
-	if request.form['insights']=='topic1':
-		tweets_per_topic = twitter.topics['Count'][0]
-		selected_topic = twitter.topics.index[0]
-	elif request.form['insights']=='topic2':
-		tweets_per_topic = twitter.topics['Count'][1]
-		selected_topic = twitter.topics.index[1]
-	elif request.form['insights']=='topic3':
-		tweets_per_topic = twitter.topics['Count'][2]
-		selected_topic = twitter.topics.index[2]
-	elif request.form['insights']=='topic4':
-		tweets_per_topic = twitter.topics['Count'][3]
-		selected_topic = twitter.topics.index[3]
-	else:
-		tweets_per_topic = twitter.topics['Count'][4]
-		selected_topic = twitter.topics.index[4]
+    # Plot WordCloud
+    LOW = twitter.create_LOW(selected_topic)
+    fig1 = twitter.create_wordcloud(LOW)
 
-	# Plot WordCloud
-	LOW = twitter.create_LOW(selected_topic)
-	fig1 = twitter.create_wordcloud(LOW)
+    # Plot polarity
+    fig2 = twitter.polarity_plot(selected_topic)
 
-	# Plot polarity
-	fig2 = twitter.polarity_plot(selected_topic)
+    # Plot objectivity
+    fig3 = twitter.objectivity_plot(selected_topic)
 
-	# Plot objectivity
-	fig3 = twitter.objectivity_plot(selected_topic)
+    return render_template('moreinsights.html', num_of_tweets=app.vars['num_of_tweets'],
+                           num_of_tweets_per_topic=tweets_per_topic, topic=selected_topic, wordcloud=fig1,
+                           polarity=fig2, objectivity=fig3)
 
-	return render_template('moreinsights.html',num_of_tweets=app.vars['num_of_tweets'], 
-		num_of_tweets_per_topic=tweets_per_topic, topic=selected_topic,wordcloud=fig1,
-		polarity=fig2, objectivity=fig3)
 
-	
-@app.route('/error',methods=["GET","POST"])
+@app.route('/error', methods=["GET", "POST"])
 def error():
-	if request.method=="POST":
-		clean_search()
-		return redirect('/')
-	else:
-		return render_template('error.html')
+    if request.method == "POST":
+        clean_search()
+        return redirect('/')
+    else:
+        return render_template('error.html')
+
 
 def clean_search():
-	app.vars={}
-	return None
+    app.vars = {}
+    return None
+
 
 if __name__ == '__main__':
-	app.run(debug=True) 
+    app.run(debug=True)
